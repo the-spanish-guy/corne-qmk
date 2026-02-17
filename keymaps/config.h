8fcd924f5003fee7804509847cc1f17875282c04
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2026-02-16 22:19:37 -0300

    chore: improve config

--- keymaps/config.h
+++ keymaps/config.h
@@ -18,38 +18,61 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+// ──────────────────────────────────────────────
+// VIAL
+// ──────────────────────────────────────────────
 #define VIAL_KEYBOARD_UID {0x3B, 0x6B, 0xA0, 0x29, 0x80, 0x56, 0xED, 0xD1}
 #define VIAL_UNLOCK_COMBO_ROWS {0, 0}
 #define VIAL_UNLOCK_COMBO_COLS {0, 1}
 
 #undef DYNAMIC_KEYMAP_LAYER_COUNT
 #define DYNAMIC_KEYMAP_LAYER_COUNT 4
-#define TAPPING_TERM 180
 
-//#define USE_MATRIX_I2C
-#ifdef KEYBOARD_crkbd_rev1_legacy
-#    undef USE_I2C
-#    define USE_SERIAL
-#endif
+// ──────────────────────────────────────────────
+// TAP-HOLD
+// ──────────────────────────────────────────────
+#define TAPPING_TERM 200
+#define QUICK_TAP_TERM 180
+#define PERMISSIVE_HOLD
 
-/* Select hand configuration */
-
+// ──────────────────────────────────────────────
+// SPLIT KEYBOARD
+// ──────────────────────────────────────────────
 #define MASTER_LEFT
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
 #define USE_SERIAL_PD2
+
+// ──────────────────────────────────────────────
+// RGB UNDERGLOW - TODOS OS EFEITOS!
+// ──────────────────────────────────────────────
 #ifdef RGBLIGHT_ENABLE
 #    undef RGBLIGHT_LED_COUNT
-#    define RGBLIGHT_ANIMATIONS
 #    define RGBLIGHT_LED_COUNT 54
 #    undef RGBLED_SPLIT
-#    define RGBLED_SPLIT \
-        { 27, 27 }
-#    define RGBLIGHT_LIMIT_VAL 120
+#    define RGBLED_SPLIT { 27, 27 }
+    
+#    define RGBLIGHT_LIMIT_VAL 255  // Brilho máximo (0-255)
 #    define RGBLIGHT_HUE_STEP  10
 #    define RGBLIGHT_SAT_STEP  17
 #    define RGBLIGHT_VAL_STEP  17
+    
+    // Habilitando TODOS os efeitos RGB
+#    define RGBLIGHT_EFFECT_ALTERNATING
+#    define RGBLIGHT_EFFECT_BREATHING
+#    define RGBLIGHT_EFFECT_CHRISTMAS
+#    define RGBLIGHT_EFFECT_KNIGHT
+#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#    define RGBLIGHT_EFFECT_RGB_TEST
+#    define RGBLIGHT_EFFECT_SNAKE
+#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+#    define RGBLIGHT_EFFECT_TWINKLE
 #endif
 
+// ──────────────────────────────────────────────
+// OLED
+// ──────────────────────────────────────────────
 #define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
+#define OLED_TIMEOUT 60000  // 60 segundos
--- keymaps/rules.mk
+++ keymaps/rules.mk
@@ -2,18 +2,24 @@ VIA_ENABLE          = yes
 VIAL_ENABLE         = yes
 LTO_ENABLE          = yes
 
+# RGB Underglow com TODOS os efeitos
 RGBLIGHT_ENABLE     = yes
 RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
+
+# OLED habilitado
+OLED_ENABLE         = yes
+OLED_DRIVER         = ssd1306
+
+# Features extras
 MOUSEKEY_ENABLE     = no
-OLED_ENABLE         = no
-OLED_DRIVER         = SSD1306
-EXTRAKEY_ENABLE     = no
+EXTRAKEY_ENABLE     = yes  # Habilitando para media keys funcionarem
 COMBO_ENABLE        = no
+QMK_SETTINGS        = yes  # Habilitando para ajustar configs via Vial
 
-QMK_SETTINGS        = no
+# Features adicionais úteis
+CAPS_WORD_ENABLE    = yes
+LAYER_LOCK_ENABLE   = no
+REPEAT_KEY_ENABLE   = no
 
-CAPS_WORD_ENABLE = no
-LAYER_LOCK_ENABLE = no
-REPEAT_KEY_ENABLE = no
-OLED_ENABLE = yes
-OLED_DRIVER = ssd1306
+# Dynamic keymap obrigatório para Vial
+DYNAMIC_KEYMAP_ENABLE = yes
