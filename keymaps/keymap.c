6dcaddfd6e0e8fd37f34986d3ab139919065c752
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2026-02-17 08:06:32 -0300

    chore: showing rgb effect name on oled screen

--- keymaps/keymap.c
+++ keymaps/keymap.c
@@ -29,7 +29,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, TL_LOWR,  KC_SPC,     KC_ENT, TL_UPPR, KC_RALT
                                       //`--------------------------'  `--------------------------'
-
   ),
 
   [1] = LAYOUT_split_3x6_3(
@@ -60,9 +59,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
         QK_BOOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_TOG, RGB_HUI, RGB_SAI, RGB_VAI, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+       RM_TOGG, RM_HUEU, RM_SATU, RM_VALU, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_MOD, RGB_HUD, RGB_SAD, RGB_VAD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+       RM_NEXT, RM_HUED, RM_SATD, RM_VALD, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           KC_LGUI, _______,  KC_SPC,     KC_ENT, _______, KC_RALT
                                       //`--------------------------'  `--------------------------'
@@ -73,38 +72,83 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #include <stdio.h>
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
-  if (!is_keyboard_master()) {
-    return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
-  }
-  return rotation;
+    if (!is_keyboard_master()) {
+        return OLED_ROTATION_180;
+    }
+    return rotation;
 }
 
-#define L_BASE 0
-#define L_LOWER 2
-#define L_RAISE 4
-#define L_ADJUST 8
-
-void oled_render_layer_state(void) {
-    oled_write_P(PSTR("Layer: "), false);
-    switch (layer_state) {
-        case L_BASE:
-            oled_write_ln_P(PSTR("Default"), false);
-            break;
-        case L_LOWER:
-            oled_write_ln_P(PSTR("Lower"), false);
-            break;
-        case L_RAISE:
-            oled_write_ln_P(PSTR("Raise"), false);
-            break;
-        case L_ADJUST:
-        case L_ADJUST|L_LOWER:
-        case L_ADJUST|L_RAISE:
-        case L_ADJUST|L_LOWER|L_RAISE:
-            oled_write_ln_P(PSTR("Adjust"), false);
-            break;
+// Nomes dos efeitos RGB Matrix
+static const char* rgb_matrix_effect_name(void) {
+    switch (rgb_matrix_get_mode()) {
+        case RGB_MATRIX_NONE:                       return "Off            ";
+        case RGB_MATRIX_SOLID_COLOR:                return "Solid          ";
+        case RGB_MATRIX_ALPHAS_MODS:                return "Alphas Mods    ";
+        case RGB_MATRIX_GRADIENT_UP_DOWN:           return "Gradient UD    ";
+        case RGB_MATRIX_GRADIENT_LEFT_RIGHT:        return "Gradient LR    ";
+        case RGB_MATRIX_BREATHING:                  return "Breathing      ";
+        case RGB_MATRIX_BAND_SAT:                   return "Band Sat       ";
+        case RGB_MATRIX_BAND_VAL:                   return "Band Val       ";
+        case RGB_MATRIX_BAND_PINWHEEL_SAT:          return "Pinwheel Sat   ";
+        case RGB_MATRIX_BAND_PINWHEEL_VAL:          return "Pinwheel Val   ";
+        case RGB_MATRIX_BAND_SPIRAL_SAT:            return "Spiral Sat     ";
+        case RGB_MATRIX_BAND_SPIRAL_VAL:            return "Spiral Val     ";
+        case RGB_MATRIX_CYCLE_ALL:                  return "Cycle All      ";
+        case RGB_MATRIX_CYCLE_LEFT_RIGHT:           return "Cycle LR       ";
+        case RGB_MATRIX_CYCLE_UP_DOWN:              return "Cycle UD       ";
+        case RGB_MATRIX_RAINBOW_MOVING_CHEVRON:     return "Chevron        ";
+        case RGB_MATRIX_CYCLE_OUT_IN:               return "Cycle Out In   ";
+        case RGB_MATRIX_CYCLE_OUT_IN_DUAL:          return "Cycle OI Dual  ";
+        case RGB_MATRIX_CYCLE_PINWHEEL:             return "Cycle Pinwheel ";
+        case RGB_MATRIX_CYCLE_SPIRAL:               return "Cycle Spiral   ";
+        case RGB_MATRIX_DUAL_BEACON:                return "Dual Beacon    ";
+        case RGB_MATRIX_RAINBOW_BEACON:             return "Rainbow Beacon ";
+        case RGB_MATRIX_RAINBOW_PINWHEELS:          return "Pinwheels      ";
+        case RGB_MATRIX_RAINDROPS:                  return "Raindrops      ";
+        case RGB_MATRIX_JELLYBEAN_RAINDROPS:        return "Jellybean      ";
+        case RGB_MATRIX_HUE_BREATHING:              return "Hue Breathing  ";
+        case RGB_MATRIX_HUE_PENDULUM:               return "Hue Pendulum   ";
+        case RGB_MATRIX_HUE_WAVE:                   return "Hue Wave       ";
+        case RGB_MATRIX_PIXEL_FRACTAL:              return "Pixel Fractal  ";
+        case RGB_MATRIX_PIXEL_FLOW:                 return "Pixel Flow     ";
+        case RGB_MATRIX_PIXEL_RAIN:                 return "Pixel Rain     ";
+        case RGB_MATRIX_TYPING_HEATMAP:             return "Heatmap        ";
+        case RGB_MATRIX_DIGITAL_RAIN:               return "Digital Rain   ";
+        case RGB_MATRIX_SOLID_REACTIVE_SIMPLE:      return "Reactive Simple";
+        case RGB_MATRIX_SOLID_REACTIVE:             return "Reactive       ";
+        case RGB_MATRIX_SOLID_REACTIVE_WIDE:        return "Reactive Wide  ";
+        case RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE:   return "React MultiWide";
+        case RGB_MATRIX_SOLID_REACTIVE_CROSS:       return "Reactive Cross ";
+        case RGB_MATRIX_SOLID_REACTIVE_MULTICROSS:  return "React MultiCros";
+        case RGB_MATRIX_SOLID_REACTIVE_NEXUS:       return "Reactive Nexus ";
+        case RGB_MATRIX_SOLID_REACTIVE_MULTINEXUS:  return "React MultiNexs";
+        case RGB_MATRIX_SPLASH:                     return "Splash         ";
+        case RGB_MATRIX_MULTISPLASH:                return "Multi Splash   ";
+        case RGB_MATRIX_SOLID_SPLASH:               return "Solid Splash   ";
+        case RGB_MATRIX_SOLID_MULTISPLASH:          return "Solid MultiSpls";
+        default:                                    return "Unknown        ";
     }
 }
 
+void oled_render_layer_state(void) {
+    oled_write_P(PSTR("Layer: "), false);
+    switch (get_highest_layer(layer_state)) {
+        case 0: oled_write_ln_P(PSTR("Base   "), false); break;
+        case 1: oled_write_ln_P(PSTR("Lower  "), false); break;
+        case 2: oled_write_ln_P(PSTR("Raise  "), false); break;
+        case 3: oled_write_ln_P(PSTR("Adjust "), false); break;
+        default: oled_write_ln_P(PSTR("???    "), false); break;
+    }
+}
+
+void oled_render_rgb_status(void) {
+    oled_write_P(PSTR("RGB: "), false);
+    if (rgb_matrix_is_enabled()) {
+        oled_write_ln(rgb_matrix_effect_name(), false);
+    } else {
+        oled_write_ln_P(PSTR("Off            "), false);
+    }
+}
 
 char keylog_str[24] = {};
 
@@ -117,36 +161,21 @@ const char code_to_name[60] = {
     '#', ';', '\'', '`', ',', '.', '/', ' ', ' ', ' '};
 
 void set_keylog(uint16_t keycode, keyrecord_t *record) {
-  char name = ' ';
+    char name = ' ';
     if ((keycode >= QK_MOD_TAP && keycode <= QK_MOD_TAP_MAX) ||
-        (keycode >= QK_LAYER_TAP && keycode <= QK_LAYER_TAP_MAX)) { keycode = keycode & 0xFF; }
-  if (keycode < 60) {
-    name = code_to_name[keycode];
-  }
-
-  // update keylog
-  snprintf(keylog_str, sizeof(keylog_str), "%dx%d, k%2d : %c",
-           record->event.key.row, record->event.key.col,
-           keycode, name);
+        (keycode >= QK_LAYER_TAP && keycode <= QK_LAYER_TAP_MAX)) {
+        keycode = keycode & 0xFF;
+    }
+    if (keycode < 60) {
+        name = code_to_name[keycode];
+    }
+    snprintf(keylog_str, sizeof(keylog_str), "%dx%d, k%2d : %c",
+             record->event.key.row, record->event.key.col,
+             keycode, name);
 }
 
 void oled_render_keylog(void) {
     oled_write(keylog_str, false);
-}
-
-void render_bootmagic_status(bool status) {
-    /* Show Ctrl-Gui Swap options */
-    static const char PROGMEM logo[][2][3] = {
-        {{0x97, 0x98, 0}, {0xb7, 0xb8, 0}},
-        {{0x95, 0x96, 0}, {0xb5, 0xb6, 0}},
-    };
-    if (status) {
-        oled_write_ln_P(logo[0][0], false);
-        oled_write_ln_P(logo[0][1], false);
-    } else {
-        oled_write_ln_P(logo[1][0], false);
-        oled_write_ln_P(logo[1][1], false);
-    }
 }
 
 void oled_render_logo(void) {
@@ -161,6 +190,7 @@ void oled_render_logo(void) {
 bool oled_task_user(void) {
     if (is_keyboard_master()) {
         oled_render_layer_state();
+        oled_render_rgb_status();  // Mostra efeito RGB atual!
         oled_render_keylog();
     } else {
         oled_render_logo();
@@ -169,9 +199,9 @@ bool oled_task_user(void) {
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  if (record->event.pressed) {
-    set_keylog(keycode, record);
-  }
-  return true;
+    if (record->event.pressed) {
+        set_keylog(keycode, record);
+    }
+    return true;
 }
 #endif // OLED_ENABLE
