1713ed8978b533058daa80becbc9e36e8959e325
Luis Enrique Meza Paredes <luyo.lemp@gmail.com>
2026-02-16 22:23:11 -0300

    chore: improve config

--- keymaps/rules.mk
+++ keymaps/rules.mk
@@ -2,9 +2,9 @@ VIA_ENABLE          = yes
 VIAL_ENABLE         = yes
 LTO_ENABLE          = yes
 
-# RGB Underglow com TODOS os efeitos
-RGBLIGHT_ENABLE     = yes
-RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
+# RGB MATRIX com TODOS os efeitos (controla LEDs das teclas + underglow)
+RGB_MATRIX_ENABLE   = yes
+RGBLIGHT_ENABLE     = no  # Não pode ter os dois ao mesmo tempo
 
 # OLED habilitado
 OLED_ENABLE         = yes
@@ -12,11 +12,11 @@ OLED_DRIVER         = ssd1306
 
 # Features extras
 MOUSEKEY_ENABLE     = no
-EXTRAKEY_ENABLE     = yes  # Habilitando para media keys funcionarem
+EXTRAKEY_ENABLE     = yes  # Media keys
 COMBO_ENABLE        = no
-QMK_SETTINGS        = yes  # Habilitando para ajustar configs via Vial
+QMK_SETTINGS        = yes
 
-# Features adicionais úteis
+# Features adicionais
 CAPS_WORD_ENABLE    = yes
 LAYER_LOCK_ENABLE   = no
 REPEAT_KEY_ENABLE   = no
